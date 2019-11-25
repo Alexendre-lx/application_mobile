769d36f0532c618b384abcd80336e7e7c5e7a8d8
Alexendre <alexendre.o@outlook.fr>
2019-11-25 13:15:07 +0100

    first commit

---
+++ Regale'app/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  Regale'app
+//
+//  Created by ALEXENDRE OBLI on 29/10/2019.
+//  Copyright © 2019 Alexendre. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
